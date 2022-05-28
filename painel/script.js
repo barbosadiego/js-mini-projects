8747e19ca769b611c7b42b2c13116a0bfa5401a0
barbosadiego <diego.gutemberg@hotmail.com>
2022-05-28 16:28:05 -0300

    painel

---
+++ painel/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const list = document.querySelector('.js-list');
+
+const createItem = (item) => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.innerHTML = `
+    <img src="${item.img}" alt="">
+    <a href="${item.url}"> Ver projeto </a>
+  `;
+  list.appendChild(li);
+};
+
+const items = async () => {
+  const data = await fetch('./painel/items.json');
+  const dataArray = await data.json();
+  dataArray.forEach((item) => createItem(item));
+};
+
+items();
